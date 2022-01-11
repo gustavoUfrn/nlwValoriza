daea075445a4c78f218dfd3b1935f161727a166c
Gustavo Alessandro <mxgugaa@gmail.com>
2022-01-11 04:22:30 -1200

    Third commit

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
+import { router } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+
 import "./database"
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: "Internal Server Error"
+    })
+  }
+})
 
 app.listen(3333, () => console.log("Servidor rodando!!"));
